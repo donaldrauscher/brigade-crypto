f8f2a9aff65cd5110d26bc769151ea0790f7d29b
Donald Rauscher <donald.rauscher@gmail.com>
2018-07-08 14:37:20 -0500

    Bash doesn't expand stuff in single quotes

--- test.js
+++ test.js
@@ -3,12 +3,15 @@ const { events, Job } = require("brigadier")
 events.on("exec", (e, p) => {
   var j1 = new Job("download", "gcr.io/cloud-builders/gcloud:latest")
 
+  j1.storage.enabled = false
+
   j1.env = {
     "COIN_API_KEY": p.secrets.coinAPIKey
   }
 
   j1.tasks = [
-    "curl https://rest.coinapi.io/v1/quotes/current?filter_symbol_id=_USD --request GET --header 'X-CoinAPI-Key: $COIN_API_KEY' -o quotes.json",
+    "set -o xtrace",
+    "curl https://rest.coinapi.io/v1/quotes/current?filter_symbol_id=_SPOT_ --request GET --header \"X-CoinAPI-Key: $COIN_API_KEY\" -o quotes.json",
     "gsutil cp quotes.json gs://djr-data/crypto/"
   ]
 
