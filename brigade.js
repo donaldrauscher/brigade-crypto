864fcc44200fa7d5da7189229457e96365c93dc7
Donald Rauscher <donald.rauscher@gmail.com>
2018-07-12 08:39:17 -0500

    Pass job timestamp in payload

--- brigade.js
+++ brigade.js
@@ -28,14 +28,16 @@ events.on("exec", (e, p) => {
   j1.storage.enabled = false
 
   j1.env = {
-    "COIN_API_KEY": p.secrets.coinAPIKey
+    "COIN_API_KEY": p.secrets.coinAPIKey,
+    "TIMESTAMP": e.payload || 'YYYY-MM-DD HH:MM'
   }
 
   j1.tasks = [
+    "set -x",
     "curl https://rest.coinapi.io/v1/quotes/current?filter_symbol_id=_SPOT_ --request GET --header \"X-CoinAPI-Key: $COIN_API_KEY\" -o quotes.json",
     "jq --compact-output '.[]' quotes.json > quotes.ndjson",
-    "gsutil cp quotes.ndjson gs://djr-data/crypto/",
-    "bq load --replace --source_format=NEWLINE_DELIMITED_JSON crypto.quotes gs://djr-data/crypto/quotes.ndjson"
+    "gsutil cp quotes.ndjson \"gs://djr-data/crypto/$TIMESTAMP/\"",
+    "bq load --replace --source_format=NEWLINE_DELIMITED_JSON crypto.quotes \"gs://djr-data/crypto/$TIMESTAMP/quotes.ndjson\""
   ]
 
   j1.run()
